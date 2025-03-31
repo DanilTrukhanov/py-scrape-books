90615c6169f82cff1885f6cdc02f3762da32c1af
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-31 11:49:34 +0300

    Solution

--- books/spiders/bookspider.py
+++ books/spiders/bookspider.py
@@ -6,5 +6,34 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rating = {
+            "One": 1,
+            "Two": 2,
+            "Three": 3,
+            "Four": 4,
+            "Five": 5,
+        }
+
+
     def parse(self, response):
-        pass
+        for book in response.css("article.product_pod"):
+            detail_page_url = book.css("h3 > a::attr(href)").get()
+            yield response.follow(detail_page_url, callback=self.parse_book)
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+
+    def parse_book(self, response):
+        yield {
+            "title": response.css("h1::text").get(),
+            "price": float(response.css("p.price_color::text").get().strip("£")),
+            "amount_in_stock": int(response.css("p.instock.availability").get().split()[-3].strip("(")),
+            "rating": self.rating.get(response.css("p.star-rating::attr(class)").get().split()[-1]),
+            "category": response.css("li a::text").getall()[2],
+            "description": response.css("div.sub-header + p::text").get(),
+            "upc": response.css("td::text").getall()[0]
+        }
