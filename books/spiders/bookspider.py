8c133039d41c9d65c85f7083c1f2ccbf321fd6f5
Danylo Trukhanov <111445038+DanilTrukhanov@users.noreply.github.com>
2025-03-31 11:52:59 +0300

    Solution

--- books/spiders/bookspider.py
+++ books/spiders/bookspider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http import Response
 
 
 class BookspiderSpider(scrapy.Spider):
@@ -6,7 +7,7 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.rating = {
             "One": 1,
@@ -16,8 +17,7 @@ class BookspiderSpider(scrapy.Spider):
             "Five": 5,
         }
 
-
-    def parse(self, response):
+    def parse(self, response: Response) -> dict:
         for book in response.css("article.product_pod"):
             detail_page_url = book.css("h3 > a::attr(href)").get()
             yield response.follow(detail_page_url, callback=self.parse_book)
@@ -26,13 +26,21 @@ class BookspiderSpider(scrapy.Spider):
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
 
-
-    def parse_book(self, response):
+    def parse_book(self, response: Response) -> dict:
         yield {
             "title": response.css("h1::text").get(),
-            "price": float(response.css("p.price_color::text").get().strip("£")),
-            "amount_in_stock": int(response.css("p.instock.availability").get().split()[-3].strip("(")),
-            "rating": self.rating.get(response.css("p.star-rating::attr(class)").get().split()[-1]),
+            "price": float(
+                response.css("p.price_color::text").get().strip("£")
+            ),
+            "amount_in_stock": int(
+                response.css(
+                    "p.instock.availability"
+                ).get().split()[-3].strip("(")
+            ),
+            "rating": self.rating.get(
+                response.css(
+                    "p.star-rating::attr(class)"
+                ).get().split()[-1]),
             "category": response.css("li a::text").getall()[2],
             "description": response.css("div.sub-header + p::text").get(),
             "upc": response.css("td::text").getall()[0]
